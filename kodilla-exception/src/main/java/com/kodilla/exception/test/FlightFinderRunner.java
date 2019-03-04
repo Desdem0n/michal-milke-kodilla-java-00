f9a7ca8ed150bc7e951f62b5f75a7c2fdf3d1d5a
Desdem0n <39797856+Desdem0n@users.noreply.github.com>
2019-03-04 15:24:09 +0100

    FlightFinderRunner.java
    task 8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
@@ -0,0 +1,18 @@
+package com.kodilla.exception.test;
+public class FlightFinderRunner {
+    public static void main(String args[]) {
+
+        Flight flight = new Flight("Warsaw", "Gdansk");
+
+        FlightFinder flightFinder = new FlightFinder();
+
+        try {
+
+            flightFinder.findFlight(flight);
+
+        } catch (RouteNotFoundException e) {
+
+            System.out.println("The airport you are trying to find doesn't exist!");
+        }
+    }
+}
